6142ca86c2d5e90b3a0b9a3467397b2d52a746a0
Vikram Panduranga <vpandura@codeaurora.org>
2021-03-24 10:31:39 -0700

    dsp: remove export symbol for static functions
    Export symbol cannot be added for static functions.
    
    Change-Id: Ief55b7868f2c16e5a11681e24432561d45765e52
    Signed-off-by: Vikram Panduranga <vpandura@codeaurora.org>

--- dsp/msm_audio_ion.c
+++ dsp/msm_audio_ion.c
@@ -534,7 +534,6 @@ err:
 	*dma_buf = NULL;
 	return rc;
 }
-EXPORT_SYMBOL(msm_audio_ion_import);
 
 /**
  * msm_audio_ion_free -
@@ -563,7 +562,6 @@ static int msm_audio_ion_free(struct dma_buf *dma_buf, struct msm_audio_ion_priv
 
 	return 0;
 }
-EXPORT_SYMBOL(msm_audio_ion_free);
 
 /**
  * msm_audio_ion_crash_handler -
