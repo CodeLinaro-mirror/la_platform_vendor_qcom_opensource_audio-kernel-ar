149fc9042f312edbed9a36a0a3cd9aa76e776f59
Tejaswinee <quic_tlanghe@quicinc.com>
2022-03-10 13:08:28 +0530

    asoc: msm_common: Fix for dereferencing pointer
    In the current sequence of code, the
    kctl_pdata->id pointer may get dereferenced.
    Add a null check of the pointer before its use
    in the code sequence.
    
    Change-Id: I6b59ddbb8460079522f83bcba12979b3abc11284
    Signed-off-by: Tejaswinee Langhe <quic_tlanghe@quicinc.com>

--- asoc/msm_common.c
+++ asoc/msm_common.c
@@ -9,6 +9,37 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
+/* Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+ */
 
 #include <linux/gpio.h>
 #include <linux/of_gpio.h>
@@ -380,7 +411,7 @@ int msm_channel_map_get(struct snd_kcontrol *kcontrol,
 	struct chmap_pdata *kctl_pdata =
 			(struct chmap_pdata *)kcontrol->private_data;
 	struct snd_soc_dai *codec_dai;
-	int backend_id = kctl_pdata->id;
+	int backend_id;
 	uint32_t rx_ch[MAX_PORT], tx_ch[MAX_PORT];
 	uint32_t rx_ch_cnt = 0, tx_ch_cnt = 0;
 	uint32_t *chmap_data = NULL;
@@ -390,7 +421,7 @@ int msm_channel_map_get(struct snd_kcontrol *kcontrol,
 		pr_debug("%s: chmap_pdata is not initialized\n", __func__);
 		return -EINVAL;
 	}
-
+	backend_id = kctl_pdata->id;
 	codec_dai = kctl_pdata->dai;
 	ret = snd_soc_dai_get_channel_map(codec_dai,
 			&tx_ch_cnt, tx_ch, &rx_ch_cnt, rx_ch);
