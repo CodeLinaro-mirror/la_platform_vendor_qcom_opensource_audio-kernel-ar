2cc759903a36d178aa4e9d9444d6ab2de87073bb
Yuhui Zhao <yuhuzhao@qti.qualcomm.com>
2025-03-11 17:26:47 +0800

    asoc: mbhc: disable micbias2 first during HS removal
    Micbias2 should be disabled before L_DET is disable,
    or there could be noise in tx.
    
    Change-Id: I88503c4b513ebdf3706c6dc98bc9bfedf4bc6352
    Signed-off-by: Yuhui Zhao <quic_yuhuzhao@quicinc.com>
    (cherry picked from commit d94e045301d106e2914939022d294b6353a02e4a)

--- include/asoc/wcd-mbhc-v2.h
+++ include/asoc/wcd-mbhc-v2.h
@@ -529,6 +529,10 @@ struct wcd_mbhc_cb {
 	bool (*mbhc_get_moisture_status)(struct wcd_mbhc *mbhc);
 	void (*mbhc_moisture_polling_ctrl)(struct wcd_mbhc *mbhc, bool enable);
 	void (*mbhc_moisture_detect_en)(struct wcd_mbhc *mbhc, bool enable);
+
+	int (*mbhc_force_micbias_disable)(struct snd_soc_component *component,
+								int micb_num);
+
 };
 
 struct wcd_mbhc_fn {
