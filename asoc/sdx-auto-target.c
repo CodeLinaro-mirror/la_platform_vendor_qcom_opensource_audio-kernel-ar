044032caf0f177c20236ff015d538da4c2000b29
Meghana Reddy Mula <quic_mmula@quicinc.com>
2025-04-25 15:04:43 +0530

    asoc: add bt support for sa510m
    add support for bt audio on sa510m.
    
    Change-Id: Icbf765bf9228162c5d40ee0584430f0f731ae3ee
    Signed-off-by: Meghana Reddy Mula <quic_mmula@quicinc.com>

--- asoc/sdx-auto-target.c
+++ asoc/sdx-auto-target.c
@@ -101,10 +101,6 @@ struct msm_asoc_mach_data {
 	struct device_node *prim_master_slave_p;
 	struct device_node *sec_master_slave_p;
 	struct device_node *tert_master_slave_p;
-	void __iomem *lpass_mux_spkr_ctl_virt_addr;
-	void __iomem *lpass_sec_mux_spkr_ctl_virt_addr;
-	void __iomem *lpass_tert_mux_spkr_ctl_virt_addr;
-	void __iomem *lpass_mux_mic_ctl_virt_addr;
 };
 
 static bool is_initial_boot;
@@ -320,20 +316,12 @@ static int sdx_sec_mi2s_startup(struct snd_pcm_substream *substream)
 
 		audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x200c),  0xffff,
 		I2S_SEL << I2S_PCM_SEL_OFFSET);
-		if (pdata->lpass_sec_mux_spkr_ctl_virt_addr != NULL) {
-			if (pdata->sec_mi2s_mode == 1)
-				ret = audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x2000),
-				0xffff,SEC_TLMM_CLKS_EN_MASTER);
-                            else
-				ret = audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x2000),
-				0xffff,SEC_TLMM_CLKS_EN_SLAVE);
-		} else {
-			dev_err(card->dev, "%s: mux spkr ctl virt addr is NULL\n",
-				__func__);
-
-			ret = -EINVAL;
-			goto done;
-		}
+		if (pdata->sec_mi2s_mode == 1)
+			ret = audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x2000),
+			0xffff,SEC_TLMM_CLKS_EN_MASTER);
+		else
+			ret = audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x2000),
+			0xffff,SEC_TLMM_CLKS_EN_SLAVE);
 		/*
 		 * This sets the CONFIG PARAMETER WS_SRC.
 		 * 1 means internal clock master mode.
@@ -597,20 +585,13 @@ static int sdx_tdm_startup(struct snd_pcm_substream *substream)
 
 		audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x2008),  0xffff,
 		I2S_SEL << I2S_PCM_SEL_OFFSET);
-		if (pdata->lpass_mux_spkr_ctl_virt_addr != NULL) {
-			if (pdata->prim_mi2s_mode == 1)
-				ret = audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x2004),
-				0xffff,PRI_TLMM_CLKS_EN_MASTER);
-                            else
-				ret = audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x2004),
-				0xffff,PRI_TLMM_CLKS_EN_SLAVE);
-		} else {
-			dev_err(card->dev, "%s: mux spkr ctl virt addr is NULL\n",
-				__func__);
+		if (pdata->prim_mi2s_mode == 1)
+			ret = audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x2004),
+			0xffff,PRI_TLMM_CLKS_EN_MASTER);
+		else
+			ret = audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x2004),
+			0xffff,PRI_TLMM_CLKS_EN_SLAVE);
 
-			ret = -EINVAL;
-			goto done;
-		}
 		/*
 		 * This sets the CONFIG PARAMETER WS_SRC.
 		 * 1 means internal clock master mode.
@@ -698,20 +679,13 @@ static int sdx_auxpcm_startup(struct snd_pcm_substream *substream)
 
 	audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x2008),  0xffff,
                                             I2S_SEL << I2S_PCM_SEL_OFFSET);
-	if (pdata->lpass_mux_spkr_ctl_virt_addr != NULL) {
-		if (pdata->prim_auxpcm_mode == 1)
-			ret = audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x2004),
-			0xffff,PRI_TLMM_CLKS_EN_MASTER);
-                    else
-			ret = audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x2004),
-			0xffff,PRI_TLMM_CLKS_EN_SLAVE);
-	} else {
-		dev_err(card->dev, "%s: mux spkr ctl virt addr is NULL\n",
-			__func__);
+	if (pdata->prim_auxpcm_mode == 1)
+		ret = audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x2004),
+		0xffff,PRI_TLMM_CLKS_EN_MASTER);
+	else
+		ret = audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x2004),
+		0xffff,PRI_TLMM_CLKS_EN_SLAVE);
 
-		ret = -EINVAL;
-		goto done;
-	}
 	/*
 	 * This sets the CONFIG PARAMETER WS_SRC.
 	 * 1 means internal clock master mode.
@@ -775,20 +749,12 @@ static int sdx_sec_auxpcm_startup(struct snd_pcm_substream *substream)
 
 	audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x200c),  0xffff,
                                             I2S_SEL << I2S_PCM_SEL_OFFSET);
-	if (pdata->lpass_sec_mux_spkr_ctl_virt_addr != NULL) {
-		if (pdata->sec_auxpcm_mode == 1)
+	if (pdata->sec_auxpcm_mode == 1)
 			ret = audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x2000),
 			0xffff,SEC_TLMM_CLKS_EN_MASTER);
-                    else
+	else
 			ret = audio_prm_set_rsc_hw_csr_update((LPAIF_OFFSET + 0x2000),
 			0xffff,SEC_TLMM_CLKS_EN_SLAVE);
-	} else {
-		dev_err(card->dev, "%s: mux spkr ctl virt addr is NULL\n",
-			__func__);
-
-		ret = -EINVAL;
-		goto done;
-	}
 	/*
 	 * This sets the CONFIG PARAMETER WS_SRC.
 	 * 1 means internal clock master mode.
