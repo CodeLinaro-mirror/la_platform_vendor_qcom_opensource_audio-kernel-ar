7839f6b41346a02645ba528db36040ce5f69d2f1
Sarath Varma Ganapathiraju <quic_ganavarm@quicinc.com>
2025-10-06 16:44:24 +0530

    asoc: add support for whale soc on audio-kernel
    add support for whale soc on audio-kernel, to ensure the
    audio kernel drivers will get compiled for whale soc, though
    the lunch cmd is same.
    
    Change-Id: I353beddddb1e989073498cabde8d99a9ca46d8ae
    Signed-off-by: Sarath Varma Ganapathiraju <quic_ganavarm@quicinc.com>

--- Android.mk
+++ Android.mk
@@ -28,7 +28,7 @@ ifeq ($(call is-board-platform-in-list,sun),true)
 AUDIO_SELECT  := CONFIG_SND_SOC_SUN=m
 endif
 
-ifeq ($(call is-board-platform-in-list,canoe alor),true)
+ifeq ($(call is-board-platform-in-list,canoe alor whale),true)
 AUDIO_SELECT  := CONFIG_SND_SOC_CANOE=m
 endif
 
@@ -38,7 +38,7 @@ LOCAL_PATH := vendor/qcom/opensource/audio-kernel
 endif
 
 # Build/Package only in case of supported target
-ifeq ($(call is-board-platform-in-list,taro kalama bengal pineapple sun holi blair gen4 msmnile canoe alor), true)
+ifeq ($(call is-board-platform-in-list,taro kalama bengal pineapple sun holi blair gen4 msmnile canoe alor whale), true)
 
 # This makefile is only for DLKM
 ifneq ($(findstring vendor,$(LOCAL_PATH)),)
@@ -419,7 +419,7 @@ LOCAL_MODULE_DEBUG_ENABLE := true
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/Build_external_kernelmodule.mk
 ###########################################################
-ifeq ($(call is-board-platform-in-list, sun canoe alor),true)
+ifeq ($(call is-board-platform-in-list, sun canoe alor whale),true)
 ###########################################################
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES           := $(AUDIO_SRC_FILES)
--- audio_kernel_modules.mk
+++ audio_kernel_modules.mk
@@ -50,7 +50,7 @@ AUDIO_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/swr_dmic_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/wcd9378_dlkm.ko \
 	$(KERNEL_MODULES_OUT)/wcd9378_slave_dlkm.ko
 endif
-ifeq ($(call is-board-platform-in-list,sun canoe alor), true)
+ifeq ($(call is-board-platform-in-list,sun canoe alor whale), true)
 AUDIO_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/lpass_bt_swr_dlkm.ko
 AUDIO_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/qmp_dlkm.ko
 AUDIO_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/simple_amp_dlkm.ko
