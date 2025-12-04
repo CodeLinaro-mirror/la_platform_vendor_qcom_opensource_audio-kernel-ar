34f070fa19c261693b61f0e59fc89608297e0186
Sarath Varma Ganapathiraju <quic_ganavarm@quicinc.com>
2025-12-04 20:50:59 +0530

    soc: Swr driver support for diff swr hw-ver
    Add support for accessing SWR registers at runtime
    based on swr master hw-version, it is essential to
    support BTFM on swr version 2.0 and rest of the
    swr masters in swr version 1.7.
    
    Change-Id: Ife6e6e7d5dade03037fc7a1491c516432eaffc86
    Signed-off-by: Sarath Varma Ganapathiraju <quic_ganavarm@quicinc.com>

--- soc/swr-mstr-ctrl.h
+++ soc/swr-mstr-ctrl.h
@@ -11,6 +11,7 @@
 #include <linux/pm_qos.h>
 #include <linux/pm.h>
 #include <soc/swr-common.h>
+#include <bindings/audio-codec-port-types.h>
 
 #ifdef CONFIG_DEBUG_FS
 #include <linux/debugfs.h>
@@ -43,6 +44,7 @@
 #define SWRM_VERSION_1_6   0x01060000
 #define SWRM_VERSION_1_7   0x01070000
 #define SWRM_VERSION_2_0   0x02000000
+#define SWRM_VERSION_2_1   0x02010000
 
 #define SWR_MAX_CH_PER_PORT 8
 
@@ -152,6 +154,7 @@ struct swr_mstr_ctrl {
 	int irq;
 	int wake_irq;
 	int version;
+	int version_index;
 	int mclk_freq;
 	int bus_clk;
 	u32 num_dev;
