e41bbd6f530dae5d24b53ee8769203223f6aa472
madhav dubey <maddub@qti.qualcomm.com>
2026-02-03 10:44:44 +0530

    ASoC: machine: resolved unused variable issue
    This change resolves unused variable issue in machine driver.
    
    Change-Id: I86be5147f5c12e7e3f774e030adfa401a9a304d1
    Signed-off-by: madhav dubey <maddub@qti.qualcomm.com>

--- asoc/audio_machine.c
+++ asoc/audio_machine.c
@@ -145,6 +145,7 @@ static struct wcd_mbhc_config wcd_mbhc_cfg = {
 
 static bool msm_usbc_swap_gnd_mic(struct snd_soc_component *component, bool active)
 {
+#if IS_ENABLED(CONFIG_QCOM_WCD_USBSS_I2C)
 	int ret = 0;
 	struct snd_soc_card *card = component->card;
 	struct msm_asoc_mach_data *pdata =
@@ -153,7 +154,6 @@ static bool msm_usbc_swap_gnd_mic(struct snd_soc_component *component, bool acti
 	if (!pdata->wcd_usbss_handle)
 		return false;
 
-#if IS_ENABLED(CONFIG_QCOM_WCD_USBSS_I2C)
 	if (wcd_mbhc_cfg.usbss_hsj_connect_enable)
 		ret = wcd_usbss_switch_update(WCD_USBSS_GND_MIC_SWAP_HSJ,
 							WCD_USBSS_CABLE_CONNECT);
