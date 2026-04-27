1c3160b17b0b578ff5431e51628ee0485366f2de
Srikanth Vallakati <svallaka@qti.qualcomm.com>
2026-04-27 17:43:58 +0530

    ASoC: codecs: use vzalloc/vfree in regdump_read()
    The regdump_read() function allocates a buffer sized by the
    'count' parameter passed in from userspace (via proc_read).
    This can be a large allocation that may fail under memory
    pressure with kzalloc(). Switch to vzalloc()/vfree() which
    uses the vmalloc area and is more suitable for large,
    non-contiguous allocations.
    
    Change-Id: I8ebfadf270464d400c1f7eece9eaa27a544e41b8
    Signed-off-by: Srikanth Vallakati <svallaka@qti.qualcomm.com>

--- asoc/codecs/wsa883x/wsa883x.c
+++ asoc/codecs/wsa883x/wsa883x.c
@@ -34,6 +34,7 @@
 #include "asoc/bolero-slave-internal.h"
 #include <linux/qti-regmap-debugfs.h>
 #include <linux/proc_fs.h>
+#include <linux/vmalloc.h>
 
 #define REGDUMP_PRINT_LEN 8
 #define T1_TEMP -10
@@ -1707,7 +1708,7 @@ static int regdump_read(struct regmap *map, int baseReg, int endReg,
 	i  = ((int) *ppos + baseReg);
 
 
-	buf = kzalloc(count, GFP_KERNEL);
+	buf = vzalloc(count);
 	if (!buf)
 		return -ENOMEM;
 
@@ -1748,7 +1749,7 @@ static int regdump_read(struct regmap *map, int baseReg, int endReg,
 	if (copy_to_user(user_buf, buf, pos))
 		ret = -EFAULT;
 
-	kfree(buf);
+	vfree(buf);
 	return ret;
 }
 
